79866d3d7bca3239e3bf17d6c5116b26e918664d
Jônata Martins <jonata.martins@alunos.ifsuldeminas.edu.br>
2026-08-13 10:15:57 -0300

    Atualiza para o VITE

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,6 @@ import Container from './components/layout/Container';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 
-
-
-
 function App() {
   
 
